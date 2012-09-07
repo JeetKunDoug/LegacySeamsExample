dc05d71a01b786bccaec55df01fc85261ebb4669
JeetKunDoug <doug@therohrers.org>
2012-09-07 10:38:07 -0400

    Added unit tests using mocks for IMessageSender

--- MessageManager/MessageManagerTests.cs
+++ MessageManager/MessageManagerTests.cs
@@ -1,14 +1,38 @@
+using Moq;
 using System;
 using NUnit.Framework;
+using System.Xml.Linq;
 
 namespace MessageManager
 {
 	[TestFixture()]
-	public class MessageManagerTests
+	public class MessageManagerTests_CreateAndSendMessage
 	{
-		[Test()]
-		public void TestCase ()
+
+		int id = 1;
+		string otherData = "Some other data";
+		XDocument result;
+
+		[SetUp()]
+		public void SetUp ()
 		{
+			var sender = new Mock<IMessageSender>(MockBehavior.Strict);
+			sender.Setup (s => s.SendMessage(It.IsAny<XDocument>()))
+				.Callback<XDocument>(d => result = d);
+			var creator = new MessageCreator(sender.Object);
+			creator.CreateAndSendMessage(id, otherData);
+		}
+
+		[Test]
+		public void should_add_correct_id ()
+		{
+			Assert.That (result.Root.Attribute ("Id").Value, Is.EqualTo (this.id.ToString ()));
+		}
+
+		[Test]
+		public void should_set_other_data ()
+		{
+			Assert.That (result.Root.Element ("AdditionalData").Value, Is.EqualTo (this.otherData));
 		}
 	}
 }
